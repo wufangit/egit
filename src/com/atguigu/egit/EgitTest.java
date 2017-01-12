45a0e20e752a08b18a0e5f15d5b7070cea638776
吴帆 <911683047@qq.com>
2017-01-12 14:25:27 +0800

    update 03

--- src/com/atguigu/egit/EgitTest.java
+++ src/com/atguigu/egit/EgitTest.java
@@ -4,6 +4,11 @@ public class EgitTest {
 	
 	public static void main(String[] args) {
 		System.out.println("git。。。");
+		System.out.println("git。。。01");
+		System.out.println("git。。。01");
+		System.out.println("git。。。01");
+		System.out.println("git。。。01");
+		System.out.println("git。。。01");
 	}
 
 }
